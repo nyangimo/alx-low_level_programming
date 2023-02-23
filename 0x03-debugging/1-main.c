72a86bae13cd38128326fe42ca69a4f24b3fc8b7
nyangimo <rebeccahnyangige9@gmail,com>
2023-02-23 03:48:20 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - causes the infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+
+
+int i;
+
+printf("infinite loop incoming :(\n");
+
+i = 0;
+/*
+* while (i < 0)
+* {
+* putchar(i);
+*}
+*/
+printf("infinite loop avoided! \\o/\n");
+
+return (0);
+}
+
+
