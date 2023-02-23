a8ca13916ed6d8aaedd78618599db72ee1fcf5e3
nyangimo <rebeccahnyangige9@gmail,com>
2023-02-23 11:00:37 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * void print_numbers - prints integersfrom 0 to 9
+ * Return: No return
+ *
+ */
+
+void print_numbers(void)
+{
+int ch;
+
+for (ch = 48; ch < 58; ch++)
+{
+_putchar(ch);
+}
+
+_putchar('\n');
+}
