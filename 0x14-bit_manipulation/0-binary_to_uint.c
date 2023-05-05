49706ae21a9bd851db4575534cc00c747806659c
Nyangimo <rebeccahnyangige9@gmail.com>
2023-05-05 06:00:24 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,18 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-int c;
-unsigned int dec_val = 0;
-if (!b)
-return (0);
-for (i = 0; b[c]; c++)
-{
-if (b[c] < '0' || b[c] > '1')
-return (0);
-dec_val = 2 * dec_val + (b[c] - '0');
-}
-return (dec_val);
+int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
 }
