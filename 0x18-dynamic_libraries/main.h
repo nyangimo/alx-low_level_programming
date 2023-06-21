92d433a6373f1dda1342ccd9c17ff88981bc20df
Nyangimo <rebeccahnyangige9@gmail.com>
2023-06-21 04:48:02 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int c);
+int _isdigit(int c);
+int _isupper(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strncat(char *dest, char *src, int n);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *sl, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *s, char *accept);
+char *_strpbrk(char *haystack, char *needle);
+
+#endif /* _MAIN_H_*/
