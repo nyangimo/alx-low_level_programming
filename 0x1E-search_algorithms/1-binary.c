7245f8a85698c9329b7e028aff9a1e9cdbdf1f67
Nyangimo <rebeccahnyangige9@gmail.com>
2023-12-13 04:34:00 -0800

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,36 +1,48 @@
 #include "search_algos.h"
 
 /**
- * linear_search - To search for a value in an array
- *                 of integers using linear search.
- * @array: Pointers to the first element of the array to search.
+ * binary_search - To searche for a value in a sorted array
+ *                 of integers using binary search.
+ * @array: Points to the first element of the array to search.
  * @size: Number of elements in the array.
  * @value: Value to search for.
  *
  * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the first index where the value is located.
+ *         Otherwise, the index where the value is located.
  */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
 
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left, right;
+	
 	/* Check if the array is NULL */
 	if (array == NULL)
 		return (-1);
 
-	/* Iterate over each element of the array */
-	for (i = 0; i < size; i++)
+	/* Perform binary search */
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		/* Print the value being checked */
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
-		/* If the value is found */
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		
+		/* Calculate the middle index */
+		i = left + (right - left) / 2;
+		
+		/* If the middle element is the value */
 		if (array[i] == value)
-
-			/* Return the index of the value */
+			/* Return the index */
 			return (i);
+		
+		/* If the middle element is greater than the value */
+		if (array[i] > value)
+			/* Update the right boundary */
+			right = i - 1;
+		else
+			/* Otherwise, update the left boundary */
+			left = i + 1;
 	}
-
 	/* Value not found, return -1 */
 	return (-1);
 }
